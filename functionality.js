06b93c745227e00f598af54bf72f0982a3139967
bbleds <bledsoedesigns@gmail.com>
2015-10-29 15:08:51 -0500

    added speech functionality

--- functionality.js
+++ functionality.js
@@ -2,14 +2,29 @@ $("#translate_btn").click(function(){
 	if($("#selection").val() === "french"){
 		Language.setLexicon();
 		Language.translateToFrench();
+			var innerText = $("#translation").html();
+			var worte = new SpeechSynthesisUtterance(innerText);
+			var stimmen = window.speechSynthesis.getVoices();
+			worte.voice = stimmen[6];
+			window.speechSynthesis.speak(worte);
 		Language.clearLexicon();
 	} else if($("#selection").val() === "german"){
 		Language.setLexicon();
 		Language.translateToGerman();
+			var innerText = $("#translation").html();
+			var worte = new SpeechSynthesisUtterance(innerText);
+			var stimmen = window.speechSynthesis.getVoices();
+			worte.voice = stimmen[6];
+			window.speechSynthesis.speak(worte);
 		Language.clearLexicon();
 	} else if($("#selection").val() === "italian"){
 		Language.setLexicon();
 		Language.translateToItalian();
+			var innerText = $("#translation").html();
+			var worte = new SpeechSynthesisUtterance(innerText);
+			var stimmen = window.speechSynthesis.getVoices();
+			worte.voice = stimmen[6];
+			window.speechSynthesis.speak(worte);
 		Language.clearLexicon();
 	}
 		
